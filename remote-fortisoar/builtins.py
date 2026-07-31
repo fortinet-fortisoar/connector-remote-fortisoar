513ef4cf675660d836c17c3f901e93350ce8cc04
Naili Mahdi <39757299+naili-m@users.noreply.github.com>
2026-07-31 18:33:05 +0400

    2.1.0
    - New action: Invoke Agent
    - Changed display name to: FortiSOAR

--- remote-fortisoar/builtins.py
+++ remote-fortisoar/builtins.py
@@ -5,6 +5,6 @@ Copyright (c) 2024 Fortinet Inc
 Copyright end
 """
 
-from .operations import make_api_call, upload_file
+from .operations import make_api_call, upload_file, invoke_agent
 
-supported_operations = {'make_api_call': make_api_call, 'upload_file': upload_file}
+supported_operations = {'make_api_call': make_api_call, 'upload_file': upload_file, 'invoke_agent': invoke_agent}
